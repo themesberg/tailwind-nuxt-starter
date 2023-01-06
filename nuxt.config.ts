d0fba153abb2160f333cd3f1cdaabbff2b7386c4
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-01-06 20:08:12 +0200

    Configure Tailwind CSS.

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@nuxtjs/tailwindcss'
+    ],
+    tailwindcss: {
+        cssPath: '~/assets/css/input.css'
+    }
+})
