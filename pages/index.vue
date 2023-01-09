cb9034a2fbe63ffc0982db7da6d9b9031e3847dc
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-01-09 19:38:22 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center justify-center h-screen bg-gray-50">
           <div>
-            <h1 class="mb-6 text-4xl">Flowbite + Vue 3 + Tailwind CSS Starter</h1>
+            <h1 class="mb-6 text-4xl">Flowbite + Nuxt.js + Tailwind CSS Starter</h1>
             <div class="w-auto mb-4 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                 <NuxtLink to="/" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">Home</NuxtLink>
                 <NuxtLink to="/accordion" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">Accordion</NuxtLink>
@@ -16,7 +16,7 @@
                 <NuxtLink to="/tabs" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">Tabs</NuxtLink>
                 <NuxtLink to="/tooltip" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">Tooltip</NuxtLink>
             </div>
-            <p class="mb-8">Learn more about <a href="https://flowbite.com/docs/getting-started/vue/" class="text-blue-600 hover:underline">Flowbite + Vue 3 here</a>.</p>
+            <p class="mb-8">Learn more about <a href="https://flowbite.com/docs/getting-started/nuxt-js/" class="text-blue-600 hover:underline">Flowbite + Nuxt.js here</a>.</p>
           </div>
       </div>
   </template>
